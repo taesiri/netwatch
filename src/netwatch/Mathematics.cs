7d079001e84eb695558d2bfe53cb24a3c604f5be
Mreza Taesiri <taesiri@live.com>
2012-12-25 15:38:06 +0330

    netwatch 0.1

---
+++ src/netwatch/Mathematics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace netwatch
+{
+    public static class Mathematics
+    {
+        public static long GetUpperRounded(long value)
+        {
+            int len = value.ToString(CultureInfo.InvariantCulture).Length;
+            double returnValue = 1.0;
+            for (int i = 1; i < len; i++)
+            {
+                returnValue *= 10;
+            }
+            returnValue = (Math.Round(4*value/returnValue, MidpointRounding.AwayFromZero)*returnValue)/4;
+
+            return (long) returnValue;
+        }
+    }
+}
---
+++ src/netwatch/NetworkSettings.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace netwatch
+{
+    public class NetworkSettings
+    {
+        #region AdoptorSettings enum
+
+        public enum AdoptorSettings
+        {
+            AllTraffic,
+            LocalOnly,
+            InternetOnly,
+            Custom
+        }
+
+        #endregion
+
+        #region UnitTypes enum
+
+        public enum UnitTypes
+        {
+            KBytepers,
+            KBitepers
+        }
+
+        #endregion
+
+        public AdoptorSettings Adoptors;
+        public List<NetworkInterface> Interfaces = new List<NetworkInterface>();
+        public UnitTypes Unit;
+
+        public NetworkSettings()
+        {
+            Adoptors = AdoptorSettings.AllTraffic;
+            Unit = UnitTypes.KBytepers;
+        }
+
+        public NetworkSettings(AdoptorSettings adoptors, UnitTypes unit)
+        {
+            Adoptors = adoptors;
+            Unit = unit;
+        }
+
+        public string InterfacesToString()
+        {
+            return Adoptors.ToString();
+        }
+    }
+}
